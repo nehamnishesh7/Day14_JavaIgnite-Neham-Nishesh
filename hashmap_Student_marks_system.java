db6e1f3a54cbf7c8a33966b57ee7ddf6688612d4
rite2harini <rite2harini@gmail.com>
2026-06-10 14:58:48 +0530

    Create hashmap_Student_marks_system.java

---
+++ hashmap_Student_marks_system.java
@@ -0,0 +1,20 @@
+/*
+Student Marks Lookup System
+
+Create a program that stores the following details using a HashMap:
+
+Student ID → Student Name
+Requirements:
+Add at least 5 students.
+Display all student records.
+Ask the user to enter a Student ID.
+Display the corresponding Student Name.
+
+If the ID does not exist, print:
+
+Student Not Found
+Hint - use HashMap
+put()
+get()
+containsKey()
+  */
